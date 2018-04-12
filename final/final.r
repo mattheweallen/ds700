62359ac1568b8950b58bcdb3c4ab4543b74c27f6
matt <mattheweallen@gmail.com>
2018-04-12 03:54:48 -0500

    current version

--- final/final.r
+++ final/final.r
@@ -1,9 +1,18 @@
 # install and import imputeTS
 install.packages("imputeTS")
 library(imputeTS)
+library(zoo)
 #import dataset
 fargo_data <- read.csv("C:/Users/matt/source/repos/ds700/final/fargo.csv")
+#convert year month to date
+fargo_data$YearMonth = as.yearmon(as.character(fargo_data$YearMonth), "%Y%m")
+fargo_data$YearMonth = as.ts(fargo_data$YearMonth)
 #use impute ts to fill in NA values by Exponential Weighted Moving Average with four points on either side
-TotalExamsWithImputedVals = na.ma(fargo_data$TotalHeartExams, k = 4, weighting = "exponential")
-#TotalExamsWithImputedVals = na.interpolation(fargo_data$TotalHeartExams, option="linear")
-TotalExamsWithImputedVals
+fargo_data$TotalHeartExams = na.ma(fargo_data$TotalHeartExams, k = 4, weighting = "exponential")
+#Holt Winters 
+## Seasonal Holt-Winters
+(m <- HoltWinters(fargo_data))
+plot(fargo_data)
+plot(fitted(fargo_data))
+#ARIMA Model
+?ts
