1d081338da68119a97936d38779711765a6af469
matt <mattheweallen@gmail.com>
2018-04-12 17:11:00 -0500

    completed imputation

---
+++ final/final2.r
@@ -0,0 +1,8 @@
+# install and import imputeTS
+install.packages("imputeTS")
+library(imputeTS)
+install.packages("forecast")
+library(forecast)
+
+#import dataset
+fargo_data <- read.csv("C:/Users/matt/source/repos/ds700/final/fargo2.csv")
