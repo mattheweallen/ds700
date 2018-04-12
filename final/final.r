70d8e81ee4ddd46d3709d7af9239a74043b46e23
matt <mattheweallen@gmail.com>
2018-04-12 05:24:00 -0500

    added model scripts

--- final/final.r
+++ final/final.r
@@ -1,14 +1,15 @@
 # install and import imputeTS
 install.packages("imputeTS")
 library(imputeTS)
-library(zoo)
+#library(zoo)
 #import dataset
 fargo_data <- read.csv("C:/Users/matt/source/repos/ds700/final/fargo.csv")
 #convert year month to date
-fargo_data$YearMonth = as.yearmon(as.character(fargo_data$YearMonth), "%Y%m")
-fargo_data$YearMonth = as.ts(fargo_data$YearMonth)
+#fargo_data$YearMonth = as.yearmon(as.character(fargo_data$YearMonth), "%Y%m")
+#fargo_data$YearMonth = as.ts(fargo_data$YearMonth)
 #use impute ts to fill in NA values by Exponential Weighted Moving Average with four points on either side
 fargo_data$TotalHeartExams = na.ma(fargo_data$TotalHeartExams, k = 4, weighting = "exponential")
+write.csv(fargo_data,"C:/Users/matt/source/repos/ds700/final/fargo_imputed.csv")
 #Holt Winters 
 ## Seasonal Holt-Winters
 (m <- HoltWinters(fargo_data))
